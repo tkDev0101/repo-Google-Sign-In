b8157359ed1f73496662653e04fedf193965994d
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-06-06 20:04:42 +0200

    updated code

--- GoogleSignInMovieApp/app/src/main/java/com/example/google_signin_movieapp/SignInActivity.kt
+++ GoogleSignInMovieApp/app/src/main/java/com/example/google_signin_movieapp/SignInActivity.kt
@@ -30,6 +30,7 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
         auth = FirebaseAuth.getInstance()
 
 
